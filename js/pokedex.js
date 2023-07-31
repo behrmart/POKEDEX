33096c1a76783db10f71acd7c1176791f806ba52
behrmart <bfelipemm@gmail.com>
2023-07-31 13:58:11 -0600

    mostly finished

--- js/pokedex.js
+++ js/pokedex.js
@@ -4,6 +4,7 @@
 function boton_buscar(){
     
     let pokename = document.getElementById("pokename");
+    
     console.log(pokename.value);
     
     const loadPokemon = (id, cb) => {
@@ -37,7 +38,8 @@ function boton_buscar(){
     }
 
 
-    loadPokemon(pokename.value,(pokemon) => {
+    const pokenameLower = pokename.value.toLowerCase(); // Only accepts lowercase pokemon names
+    loadPokemon(pokenameLower,(pokemon) => {
         
         console.log(`Pokemon No.${pokemon.id} name is ${pokemon.name}`);
         let pokecard = document.getElementById("pokecard");
@@ -51,18 +53,18 @@ function boton_buscar(){
             /*Propiedad de JS para modificar contenido HTML, devuelve una cadena de texto */
         pokecard.innerHTML =    
         `
-                <div class="col-md-4">
+                <div class="col-md-4 d-flex align-items-center justify-content-center">
                     <img src="${pokemon.sprites.front_default}" class="card-img-top img-fluid mx-auto d-block " alt="Pokemon image">
                 </div>
                 <div class="col-md-8">            
                     <div class="card-header text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3 text-center"><h2>${pokemon.name}</h2></div>
                     <div class="card-body text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 text-center">
-                        <p class="card-text">Id: ${pokemon.id}</p>
+                        <p class="card-text">Pokemon Id: ${pokemon.id}</p>
                         <p class="card-text">Orden: ${pokemon.order}</p>
                         <p class="card-text">Peso: ${pokemon.weight}</p>
                         <p class="card-text"> Habilidad Principal: ${ability.name}</p>
-                        <p class="card-text"> Descripcion de Habilidad: ${ability.name}</p>
-                        <p class="card-text"> Especie: ${species.name}</p>
+                        <p class="card-text"> Descripcion de Habilidad: ${ability.effect_entries.find(entry => entry.language.name === "en").effect}</p>
+                        <p class="card-text"> Habitat: ${species.habitat.name}</p>
                         <p class="card-text"> Felicidad Base: ${species.base_happiness}</p>
                         <p class="card-text"> Rango de Captura: ${species.capture_rate}</p>
                     </div>
